8adbc1943ade0ef71e0dbc5a681e2f02f38cb6e8
Louis Popi <theh2o64@gmail.com>
2017-04-13 05:47:44 +0200

    rootdir: Most of devices provide their gps.conf
    Change-Id: If212bfd9c26d0e28c22cd05f7d724aee84db756c

--- etc/Android.mk
+++ etc/Android.mk
@@ -1,12 +1,12 @@
 ifeq (msm8916,$(QCOM_HARDWARE_VARIANT))
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
+#LOCAL_PATH := $(call my-dir)
+#include $(CLEAR_VARS)
 
-LOCAL_MODULE := gps.conf
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/
-LOCAL_SRC_FILES := gps.conf
+#LOCAL_MODULE := gps.conf
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/
+#LOCAL_SRC_FILES := gps.conf
 
-include $(BUILD_PREBUILT)
+#include $(BUILD_PREBUILT)
 endif
